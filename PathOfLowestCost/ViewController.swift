a222ade583ac5358b2afc75a3cbcecda5daf1454
Othmar Gispert <ogispert@Othmars-MacBook-Pro.local>
2017-01-19 22:27:23 -0500

    Task

--- PathOfLowestCost/ViewController.swift
+++ PathOfLowestCost/ViewController.swift
@@ -8,18 +8,147 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+   
+    @IBOutlet weak var rowOne: UITextField!
+    @IBOutlet weak var rowTwo: UITextField!
+    @IBOutlet weak var rowThree: UITextField!
+    @IBOutlet weak var rowFour: UITextField!
+    @IBOutlet weak var rowFive: UITextField!
+    @IBOutlet weak var rowSix: UITextField!
+    @IBOutlet weak var rowSeven: UITextField!
+    @IBOutlet weak var rowEight: UITextField!
+    @IBOutlet weak var rowNine: UITextField!
+    @IBOutlet weak var rowTen: UITextField!
+    
+    var rowOneArray = [Int]()
+    var rowTwoArray = [Int]()
+    var rowThreeArray = [Int]()
+    var rowFourArray = [Int]()
+    var rowFiveArray = [Int]()
+    var rowSixArray = [Int]()
+    var rowSevenArray = [Int]()
+    var rowEightArray = [Int]()
+    var rowNineArray = [Int]()
+    var rowTenArray = [Int]()
+    
+    var arrayOfMins = [Int]()
+    var arrayOfIndex = [Int]()
+    
+    var minusFifty: Bool!
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: nil)
+
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    func keyboardWillShow(notification:NSNotification){
+        
+        var userInfo = notification.userInfo!
+        var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+        
+        var contentInset:UIEdgeInsets = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height+10
+        self.scrollView.contentInset = contentInset
+    }
+    
+    func keyboardWillHide(notification:NSNotification){
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        self.scrollView.contentInset = contentInset
+    }
+    
+    func dismissKeyboard() {
+        
+        view.endEditing(true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func getResultsButton(_ sender: UIButton) {
+        
+        rowOneArray = getNumbers(label: rowOne) as! [Int]
+        rowTwoArray = getNumbers(label: rowTwo) as! [Int]
+        rowThreeArray = getNumbers(label: rowThree) as! [Int]
+        rowFourArray = getNumbers(label: rowFour) as! [Int]
+        rowFiveArray = getNumbers(label: rowFive) as! [Int]
+        rowSixArray = getNumbers(label: rowSix) as! [Int]
+        rowSevenArray = getNumbers(label: rowSeven) as! [Int]
+        rowEightArray = getNumbers(label: rowEight) as! [Int]
+        rowNineArray = getNumbers(label: rowNine) as! [Int]
+        rowTenArray = getNumbers(label: rowTen) as! [Int]
+        
+        let _ = getResults(myArray: rowOneArray)
+        let _ = getResults(myArray: rowTwoArray)
+        let _ = getResults(myArray: rowThreeArray)
+        let _ = getResults(myArray: rowFourArray)
+        let _ = getResults(myArray: rowFiveArray)
+        let _ = getResults(myArray: rowSixArray)
+        let _ = getResults(myArray: rowSevenArray)
+        let _ = getResults(myArray: rowEightArray)
+        let _ = getResults(myArray: rowNineArray)
+        let _ = getResults(myArray: rowTenArray)
+        
+        let sum = arrayOfMins.reduce(0, +)
+        
+        for i in 0...arrayOfMins.count {
+            
+            if sum <= 50 && i == arrayOfMins.count {
+                minusFifty = true
+                let alert = UIAlertController(title: "Results", message: "Yes \n \(sum) \n \(arrayOfIndex)", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title:"Ok", style: .cancel, handler: { (action: UIAlertAction!) in
+                    self.arrayOfMins.removeAll()
+                    self.arrayOfIndex.removeAll()
+                }))
+                self.present(alert, animated: true, completion: nil)
+                
+            } else if sum > 50 && i < arrayOfMins.count {
+                minusFifty = false
+                let alert = UIAlertController(title: "Results", message: "No \n \(sum) \n \(arrayOfIndex)", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title:"Ok", style: .cancel, handler: { (action: UIAlertAction!) in
+                    self.arrayOfMins.removeAll()
+                    self.arrayOfIndex.removeAll()
+                }))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
-
+    
+    func getNumbers (label: UITextField) -> Array<Any> {
+        
+        if label.text != nil {
+            let digitsString = label.text
+            let values = digitsString?.components(separatedBy: ",")
+                .flatMap {
+                    Int($0.trimmingCharacters(in: .whitespaces))
+            }
+            
+            return values!
+        }
+        
+        return []
+    }
+    
+    func getResults (myArray: Array<Int>) -> (Array<Any>,Array<Any>) {
+        
+        if myArray != [] {
+            let index = myArray.index(of: myArray.min()!)
+            arrayOfIndex.append(index!+1)
+            arrayOfMins.append(myArray.min()!)
+            return (arrayOfIndex, arrayOfMins)
+        }
+        
+        return ([],[])
+    }
 
 }
 
--- PathOfLowestCostTests/PathOfLowestCostTests.swift
+++ PathOfLowestCostTests/PathOfLowestCostTests.swift
@@ -9,11 +9,14 @@
 import XCTest
 @testable import PathOfLowestCost
 
+var controller = ViewController()
+
 class PathOfLowestCostTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
     
     override func tearDown() {
@@ -21,16 +24,44 @@ class PathOfLowestCostTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testWithYesAnswer() {
+        
+        let arrayOne = [1,2,3,4,5]
+        let arrayTwo = [5,4,3,2,1]
+        let arrayThree = [6,7,8,9,10]
+        let arrayFour = [10,9,8,7,6]
+        
+        _ = controller.getResults(myArray: arrayOne)
+        _ = controller.getResults(myArray: arrayTwo)
+        _ = controller.getResults(myArray: arrayThree)
+        _ = controller.getResults(myArray: arrayFour)
+        
+        let sum = controller.arrayOfMins.reduce(0, +)
+        let ind = controller.arrayOfIndex
+
+        XCTAssertEqual(sum, 14)
+        XCTAssertEqual(ind, [1,5,1,5])
+        
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testWithNoAnswer() {
+        
+        let arrayOne = [11,12,13,14,15]
+        let arrayTwo = [25,24,23,22,21]
+        let arrayThree = [36,37,38,39,40]
+        let arrayFour = [50,49,48,47,46]
+        
+        _ = controller.getResults(myArray: arrayOne)
+        _ = controller.getResults(myArray: arrayTwo)
+        _ = controller.getResults(myArray: arrayThree)
+        _ = controller.getResults(myArray: arrayFour)
+        
+        let sum = controller.arrayOfMins.reduce(0, +)
+        let ind = controller.arrayOfIndex
+        
+        XCTAssertEqual(sum, 114)
+        XCTAssertEqual(ind, [1,5,1,5])
+        
     }
     
 }
